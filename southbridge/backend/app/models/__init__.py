3a58e200ffb29518821ba197edd28a527b712d1f
PV Anfas <anfaspv.info@gmail.com>
2026-07-09 19:38:50 +0530

    in

--- southbridge/backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Import all models for easy access"""
-from .user import User
-from .auth import RefreshToken
-from .location import DriverLocation, DriverRouteTracking
-
-__all__ = ["User", "RefreshToken", "DriverLocation", "DriverRouteTracking"]
--- southbridge/frontend/src/hooks/useAuth.jsx
+++
@@ -1,8 +0,0 @@
-import { useSelector } from 'react-redux';
-
-const useAuth = () => {
-	const { isAuthenticated, user, accessToken, refreshToken } = useSelector(state => state.auth);
-	return { isAuthenticated, user, accessToken, refreshToken };
-};
-
-export default useAuth;
